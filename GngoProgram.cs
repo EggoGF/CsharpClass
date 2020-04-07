d554fc0fcdab6508587327b0bb5edd21500cae5f
EggoGF <63312123+EggoGF@users.noreply.github.com>
2020-04-07 11:22:55 -0700

    Add files via upload
    Upload of Week 1 Program.cs

---
+++ GngoProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello George Ngo!");
+	    Console.WriteLine("Would you like to play a game?");
+        }
+    }
+}
